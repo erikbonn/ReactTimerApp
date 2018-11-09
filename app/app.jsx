a6b7a6e8133f6c78b17ba2ef1b62de418b534e88
erikbonn <ebonn89@gmail.com>
2018-11-08 18:57:09 -0700

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+
+//load foundation:
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+// App css:
+require('style!css!sass!applicationStyles')
+
+// var objOne = {
+//   name: 'Erik',
+//   location: 'Salt Lake City'
+// };
+//
+// var objTwo = {
+//   age: 29,
+//   ...objOne
+// };
+
+// console.log(objTwo);
+
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
