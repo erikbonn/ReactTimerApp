0d9cee3c0d16d30e12502308c74c7177683fb9e8
erikbonn <ebonn89@gmail.com>
2018-11-11 17:47:29 -0700

    add informational comments about the karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -2,20 +2,20 @@ var webpackConfig = require('./webpack.config.js');
 
 module.exports = function (config) {
   config.set({
-    browsers: ['Chrome'],
-    singleRun: true,
-    frameworks: ['mocha'],
-    files: ['app/tests/**/*.test.jsx'],
-    preprocessors: {
-      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+    browsers: ['Chrome'], // specifies what browser we want to use
+    singleRun: true, // runs the test one time
+    frameworks: ['mocha'], //what framework we wanna use
+    files: ['app/tests/**/*.test.jsx'], //specifies what files we want to use
+    preprocessors: { // specifiy which tests to run
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap'] // these are preprocessors
     },
-    reporters: ['mocha'],
+    reporters: ['mocha'], // let us clean up the output
     client: {
       mocha: {
-        timeout: '5000'
+        timeout: '5000' // sends details down to the original frameworks
       }
     },
-    webpack: webpackConfig,
+    webpack: webpackConfig, // we use webpack config to create tests that use require and let us load in our modules we need
     webpackServer: {
       noInfo: true
     }
